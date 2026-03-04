791f008807f972a8c5f4b5d7b458aaf3392198bc
unknown <vayneirisu@gmail.com>
2026-03-04 23:04:57 +0700

    feat: fix mobile display

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 const whyItems = document.querySelectorAll(".why-item");
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-menu");
 
 let currentWhy = 0;
 
 function showWhy(){
 
-whyItems.forEach(item=>{
-item.classList.remove("active");
-});
+    whyItems.forEach(item=>{
+    item.classList.remove("active");
+    });
 
-whyItems[currentWhy].classList.add("active");
+    whyItems[currentWhy].classList.add("active");
 
-currentWhy++;
+    currentWhy++;
 
-if(currentWhy >= whyItems.length){
-currentWhy = 0;
-}
+    if(currentWhy >= whyItems.length){
+    currentWhy = 0;
+    }
 
 }
 
 showWhy();
 
 setInterval(showWhy,2500);
+
+toggle.addEventListener("click",()=>{
+    nav.classList.toggle("active");
+});
 
 // Smooth scroll untuk navbar
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
